7ac3b99a1bf236239619288162f020d1e8853bac
ranamikku009-cloud <ranamikku009@gmail.com>
2026-07-10 15:47:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function askAI() {
 
   output.innerHTML = "⏳ Thinking...";
 
-  const API_KEY = "rsk_01KX5QWTK21NTVKHY6TTV0K0NH";
+  const API_KEY = "rsk_01KX5RDKGVCKJGD0NVKPEBXVBS";
 
   try {
     const response = await fetch("https://api.meshy.ai/v1/chat/completions", {
